5e036a93d63a20dfdda89ef6f2358993cc66d8e5
Ana <ana.gutimandi@gmail.com>
2025-06-05 11:25:20 +0200

    Cambio engine utils

--- Inicio/utils.py
+++ Inicio/utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import geopandas as gpd
 import streamlit as st
+import os
 
 @st.cache_data
 def cargar_datos():
@@ -10,9 +11,12 @@ def cargar_datos():
 @st.cache_data
 def cargar_geodatos():
     with st.spinner('Cargando datos geográficos...'):
+        st.write("Archivos en mapa:")
+        st.write(os.listdir("Inicio/src/mapa"))
+
         try:
             # Intentar cargar el shapefile con pyogrio
-            gdf_ccaa = gpd.read_file("Inicio/src/mapa/se89_3_admin_ccaa_a_x.shp", engine="pyogrio")
+            gdf_ccaa = gpd.read_file("Inicio/src/mapa/se89_3_admin_ccaa_a_x.shp", engine="ESRI Shapefile")
         except ImportError:
             # Si pyogrio no está disponible, usar el motor por defecto
             st.warning("pyogrio no está instalado. Usando el motor por defecto para cargar los datos geográficos.")
