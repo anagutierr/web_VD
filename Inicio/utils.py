4b1d94c2e783871b00324565fc92f0f95a549e58
Ana <ana.gutimandi@gmail.com>
2025-06-05 11:31:08 +0200

    como antes

--- Inicio/utils.py
+++ Inicio/utils.py
@@ -3,8 +3,6 @@ import geopandas as gpd
 import streamlit as st
 import os
 
-os.environ["USE_PYOGRIO"] = "0"  # Forzar a usar Fiona
-
 @st.cache_data
 def cargar_datos():
     with st.spinner('Cargando datos...'):
@@ -13,8 +11,6 @@ def cargar_datos():
 @st.cache_data
 def cargar_geodatos():
     with st.spinner('Cargando datos geográficos...'):
-
         gdf_ccaa = gpd.read_file("Inicio/src/mapa/se89_3_admin_ccaa_a_x.shp")
-
         #gdf_prov = gpd.read_file("Inicio/src/mapa/recintos_provinciales_inspire_peninbal_etrs89.shp")
         return gdf_ccaa
