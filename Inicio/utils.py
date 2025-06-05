455264d4467e27cfd262b4098aa4917ba4025950
Ana <ana.gutimandi@gmail.com>
2025-06-05 11:27:38 +0200

    Use fiona utils

--- Inicio/utils.py
+++ Inicio/utils.py
@@ -3,6 +3,8 @@ import geopandas as gpd
 import streamlit as st
 import os
 
+os.environ["USE_PYOGRIO"] = "0"  # Forzar a usar Fiona
+
 @st.cache_data
 def cargar_datos():
     with st.spinner('Cargando datos...'):
@@ -11,16 +13,8 @@ def cargar_datos():
 @st.cache_data
 def cargar_geodatos():
     with st.spinner('Cargando datos geográficos...'):
-        st.write("Archivos en mapa:")
-        st.write(os.listdir("Inicio/src/mapa"))
 
-        try:
-            # Intentar cargar el shapefile con pyogrio
-            gdf_ccaa = gpd.read_file("Inicio/src/mapa/se89_3_admin_ccaa_a_x.shp", engine="ESRI Shapefile")
-        except ImportError:
-            # Si pyogrio no está disponible, usar el motor por defecto
-            st.warning("pyogrio no está instalado. Usando el motor por defecto para cargar los datos geográficos.")
-            gdf_ccaa = gpd.read_file("Inicio/src/mapa/se89_3_admin_ccaa_a_x.shp")
+        gdf_ccaa = gpd.read_file("Inicio/src/mapa/se89_3_admin_ccaa_a_x.shp")
 
         #gdf_prov = gpd.read_file("Inicio/src/mapa/recintos_provinciales_inspire_peninbal_etrs89.shp")
         return gdf_ccaa
